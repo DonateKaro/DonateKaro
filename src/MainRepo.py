f918a9cef152c6d4ce91193bb9167d406df31b4f
Ankush <lothermeyer@protonmail.com>
2022-06-02 01:04:27 +0530

    initial Commit

--- src/MainRepo.py
+++ src/MainRepo.py
@@ -4,9 +4,9 @@ import psycopg2
 class Repo:
     def __init__(self, config) -> None:
         try:
-            self.conn = psycopg2.connect(
-                user="postgres", password="password", host="127.0.0.1", port="5432", database="postgres")
-            #self.conn = psycopg2.connect(config["DATABASE_URL"])
+            #self.conn = psycopg2.connect(
+            #    user="postgres", password="password", host="127.0.0.1", port="5432", database="postgres")
+            self.conn = psycopg2.connect(config["DATABASE_URL"])
             self.cur = self.conn.cursor()
             print("Opened database successfully")
         except Exception as e:
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 class Config:
     TESTING = False
-    DATABASE_URL = ""
+    DATABASE_URL = "postgres://ecsugvbutyrcno:4bd33495ccccaed20a615cdcdc3a2b3c621ddaf4aa93bec51ade97cf41aa9d18@ec2-34-227-120-79.compute-1.amazonaws.com:5432/dafr5uav2id13j"
 
 
 class ProductionConfig(Config):
